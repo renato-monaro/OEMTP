5ae4f4779aa04f338d7cfb08b6724146d672e9a8
renato-monaro <renato.monaro@gmail.com>
2015-10-30 22:34:44 -0200

    Initial Upload

--- src/support/support.cpp
+++ src/support/support.cpp
@@ -1,3 +1,24 @@
+/**
+ *  Copyright 2015 by Renato Monaro and Silvio Giuseppe
+ *
+ * This file is part of Open Electromagnetic Transient Program - OEMTP.
+ * 
+ * OEMTP is free software: you can redistribute 
+ * it and/or modify it under the terms of the GNU General Public 
+ * License as published by the Free Software Foundation, either 
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * Some open source application is distributed in the hope that it will 
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+ */
+
 #include "support.h"
 
 double _Complex g2c(gsl_complex T){
